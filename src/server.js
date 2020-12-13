ee737f5a25731663280b7ba13fcef0de4e4f70e8
anaclaudialimacosta <almeidannynha@gmail.com>
2020-12-13 00:55:22 -0300

    Corrigindo deploy :adhesive_bandage:

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ let app = express();
 const PORT = process.env.PORT || 3002;
 
 mongoose.connect(
-    `mongodb+srv://tictactoe:${process.env.DB_PASSWORD}@cluster0.80hf3.mongodb.net/development?retryWrites=true&w=majority`, 
+    `mongodb+srv://tictactoe:${process.env.DB_PASSWORD}@cluster0.80hf3.mongodb.net/${process.env.PROFILE}?retryWrites=true&w=majority`, 
     { useNewUrlParser: true }
 );
 
